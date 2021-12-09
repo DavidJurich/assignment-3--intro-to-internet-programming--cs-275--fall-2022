e5ec9723e123387b9badd8d7916b49ca9b2e82a2
DavidJurich <davjur@gmail.com>
2021-12-09 14:38:27 -0500

    Fix entire gulp scaffolding
    Entire gulp scaffolding fixed to ensure proper tracks and final product. app.js updated to remove animation effect to fix a small bug just observed.  Update onload function for better readability.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
-const tables = document.getElementsByTagName(`td`);
-function animate() {
+window.onload = () => {
+    const tables = document.getElementsByTagName(`td`);
     for (const index of tables) {
         index.addEventListener(`click`, () => {
             for (const letter of tables) {
                 if (index === letter) {
-                    if (letter.classList.contains(`animate`))
+                    if (letter.classList.contains(`animate`)) {
                         letter.classList.add(`origin`);
-                    else
+                        letter.classList.remove(`animate`);
+                    }
+                    else {
+                        letter.classList.remove(`origin`);
                         letter.classList.add(`animate`);
-                } else if (letter.classList.contains(`animate`))
+                    }
+                } else if (letter.classList.contains(`animate`)) {
                     letter.classList.add(`origin`);
+                    letter.classList.remove(`animate`);
+                }
             }
         });
     }
-}
-
-window.onload = animate();
+};
